dd31122b3db0d9db35ebe182e8c63a781fb56ddb
ibrahiemmohamed24 <hzizo964@gmail.com>
2026-09-14 14:58:55 +0300

    feat: detect out-of-order notebook cell execution

---
+++ src/nbghost/order.py
@@ -0,0 +1,24 @@
+"""Detect notebooks where cells were executed out of their written order."""
+
+
+def find_order_issues(cells):
+    """Return a list of ordering issues found in the given notebook cells."""
+    issues = []
+    previous_count = 0
+
+    for position, cell in enumerate(cells):
+        execution_count = cell.get("execution_count")
+        if execution_count is None:
+            continue
+
+        if execution_count < previous_count:
+            issues.append(
+                {
+                    "position": position,
+                    "execution_count": execution_count,
+                }
+            )
+        else:
+            previous_count = execution_count
+
+    return issues
---
+++ tests/test_order.py
@@ -0,0 +1,25 @@
+from nbghost.order import find_order_issues
+
+
+def test_clean_notebook_has_no_issues():
+    cells = [
+        {"cell_type": "code", "execution_count": 1},
+        {"cell_type": "code", "execution_count": 2},
+        {"cell_type": "code", "execution_count": 3},
+    ]
+
+    issues = find_order_issues(cells)
+
+    assert issues == []
+
+
+def test_out_of_order_notebook_flags_the_misplaced_cell():
+    cells = [
+        {"cell_type": "code", "execution_count": 1},
+        {"cell_type": "code", "execution_count": 3},
+        {"cell_type": "code", "execution_count": 2},
+    ]
+
+    issues = find_order_issues(cells)
+
+    assert issues == [{"position": 2, "execution_count": 2}]
